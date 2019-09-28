e2d00fac2e667aa40f6657e74d1b174fe27b6f71
Mic-M <39859059+Mic-M@users.noreply.github.com>
2019-09-28 23:10:26 +0200

    Add files via upload

---
+++ words.js
@@ -0,0 +1,51 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'test adapter settings': {
+        'en': 'Adapter settings for test',
+        'de': 'Adaptereinstellungen für test',
+        'ru': 'Настройки адаптера для test',
+        'pt': 'Configurações do adaptador para test',
+        'nl': 'Adapterinstellingen voor test',
+        'fr': "Paramètres d'adaptateur pour test",
+        'it': "Impostazioni dell'adattatore per test",
+        'es': 'Ajustes del adaptador para test',
+        'pl': 'Ustawienia adaptera dla test',
+        'zh-cn': 'test的适配器设置'
+    },
+    'ownGetAdminCommands': {
+        'en': 'Your own GetAdmin Commands',
+        'de': 'Deine eigenen GetAdmin-Commands',
+    },
+    'ownGetAdminCommands_label': {
+        'en': 'GetAdmin Commands (comma separated)',
+        'de': 'GetAdmin-Commands (mit Komma trennen)',
+    },
+    'ownGetAdminCommands_info': {
+        'en': 'Enter your own commands, which you entered in the GetAdmin software in column "Command". No spaces please. Separate multiple values with comma.<br>Leave this field empty, if you have not (yet) defined any commands.',
+        'de': 'Hier die Commands eintragen, die du in der GetAdmin-Software in der Spalte "Commands" definiert hast. Bitte ohne Leerzeichen. Mehrere Werte mit Komma trennen.<br>Falls du (noch) keine eigenen Commands definiert hast, lässt du das einfach leer.',
+    },
+    'windowsDevices': {
+        'en': 'Windows devices you want to control',
+        'de': 'Zu steuernde Windows-Geräte',
+    },
+    'windowsDevices_info': {
+        'en': 'Add your Windows devices, which you want to control. "Device Name" will be used as part of the states. You can also (temporarily) de-activate devices if you want.',
+        'de': 'Hier fügst du deine Windows-Geräte hinzu, die du steuern möchtest. "Name des Gerätes" wird dabei als Teil der Datenpunkte verwendet. Du kannst auch vorübergehend Geräte auf Wunsch deaktivieren.',
+    },
+    'deviceName': {
+        'en': 'Device Name',
+        'de': 'Name des Gerätes',
+    },
+    'deviceIp': {
+        'en': 'Device IP Address',
+        'de': 'IP-Adresse des Gerätes',
+    },
+    'deviceActive': {
+        'en': 'Active?',
+        'de': 'Aktiviert?',
+    },    
+
+
+};
